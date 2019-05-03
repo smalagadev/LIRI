23cc128fe2de9ae6ca8aed0705b026483e7049dc
Steffano <smalagadev@gmail.com>
2019-05-03 01:01:15 -0400

    Rewrite init

---
+++ keys.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+  api_key: process.env.OMDB_API_KEY
+}
